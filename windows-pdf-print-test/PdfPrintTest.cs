2c68a8999afba13a2ef8ac1f209627087d712cdc
Kanat Kubash <kanatkubash@gmail.com>
2019-06-03 02:42:52 +0600

    Added test for PdfPrint class library

---
+++ windows-pdf-print-test/PdfPrintTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using windows_pdf_print;
+
+namespace windows_pdf_print_test
+{
+  [TestClass]
+  public class PdfPrintTest
+  {
+    [TestMethod]
+    public void TestInstantiatesCorrectly()
+    {
+      using (var pdfPrint = new PdfPrint())
+      {
+        Assert.IsInstanceOfType(pdfPrint, typeof(PdfPrint));
+      }
+    }
+
+    [TestMethod]
+    public void TestPrintsPdfFromDocx()
+    {
+      var dir = Directory.GetCurrentDirectory();
+      var output = $"{dir}\\sample.pdf";
+
+      if (File.Exists(output))
+        File.Delete(output);
+
+      using (var pdfPrint = new PdfPrint())
+      {
+        pdfPrint.Print($"{dir}\\sample.docx", output);
+        Assert.IsTrue(File.Exists(output));
+      }
+    }
+
+    [TestMethod, ExpectedException(typeof(FileNotFoundException))]
+    public void TestThrowsIfFileNotFound()
+    {
+      using (var pdfPrint = new PdfPrint())
+      {
+        pdfPrint.Print("C:\\nonexisting-file.nonexistent-extension", "out.pdf");
+      }
+    }
+
+    [TestMethod, ExpectedException(typeof(InvalidOperationException))]
+    public void TestThrowsIfPdfPrinterNotFound()
+    {
+      var dir = Directory.GetCurrentDirectory();
+
+      using (var pdfPrint = new PdfPrint(pdfPrinterName: "Nonexistent PDF printer"))
+      {
+        pdfPrint.Print($"{dir}\\sample.docx", "out.pdf");
+      }
+    }
+  }
+}
---
+++ windows-pdf-print-test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("windows-pdf-print-test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("windows-pdf-print-test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c4251b8b-3d23-4f55-a576-9c850536cf34")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
